fb752bc3d8d431a657d9af0d98d31f9e94e80e63
irerin07 <counterchord89@gmail.com>
2023-10-06 17:14:36 +0900

    #2 DTO 생성 - User DTO 생성

---
+++ src/main/java/com/tistory/irerin07/wantedpreonboardingbackend/domain/dto/UserDto.java
@@ -0,0 +1,9 @@
+package com.tistory.irerin07.wantedpreonboardingbackend.domain.dto;
+
+/**
+ * @author 민경수
+ * @description user dto
+ * @since 2023.10.06
+ **********************************************************************************************************************/
+public class UserDto {
+}
