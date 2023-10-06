e6f6d56841af7e5c031ac7562c93db0979114763
irerin07 <counterchord89@gmail.com>
2023-10-06 13:00:08 +0900

    Initial commit

---
+++ src/main/java/com/tistory/irerin07/wantedpreonboardingbackend/WantedPreOnboardingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tistory.irerin07.wantedpreonboardingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedPreOnboardingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WantedPreOnboardingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tistory/irerin07/wantedpreonboardingbackend/WantedPreOnboardingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistory.irerin07.wantedpreonboardingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedPreOnboardingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
