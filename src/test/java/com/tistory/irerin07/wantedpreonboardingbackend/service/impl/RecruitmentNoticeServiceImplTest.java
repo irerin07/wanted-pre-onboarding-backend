f27f3c6337fbb99f968e8617d1e76998785600ae
irerin07 <counterchord89@gmail.com>
2023-10-09 18:22:39 +0900

    #19 코드 리팩터링 - set, modify 서비스 메서드 각각 엔티티 반환하도록 메소드 수정 - 테스트 코드 추가

--- src/test/java/com/tistory/irerin07/wantedpreonboardingbackend/service/impl/RecruitmentNoticeServiceImplTest.java
+++ src/test/java/com/tistory/irerin07/wantedpreonboardingbackend/service/impl/RecruitmentNoticeServiceImplTest.java
@@ -1,44 +1,79 @@
 package com.tistory.irerin07.wantedpreonboardingbackend.service.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 
-import org.junit.jupiter.api.DisplayName;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 
 import com.tistory.irerin07.wantedpreonboardingbackend.domain.entity.Company;
 import com.tistory.irerin07.wantedpreonboardingbackend.domain.entity.RecruitmentNotice;
+import com.tistory.irerin07.wantedpreonboardingbackend.domain.vo.RecruitmentNoticeVo;
 import com.tistory.irerin07.wantedpreonboardingbackend.repository.RecruitmentNoticeRepository;
-import com.tistory.irerin07.wantedpreonboardingbackend.service.RecruitmentNoticeService;
+import com.tistory.irerin07.wantedpreonboardingbackend.service.CompanyService;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 class RecruitmentNoticeServiceImplTest {
 
-  @Mock
+  @MockBean
   private RecruitmentNoticeRepository repository;
 
-  @InjectMocks
+  @MockBean
+  private CompanyService companyService;
+
+  @SpyBean
   private RecruitmentNoticeServiceImpl service;
 
-  @Test
-  @DisplayName("채용공고 등록시 필요 데이터 누락")
-  void missingData() {
+  private final Company mockCompany = mock(Company.class);
+
+  private final RecruitmentNotice recruitmentNotice =
+    RecruitmentNotice.builder().recruitDescription("채용 내용").requiredSkill("Java").recruitReward(120000).jobPosition("백엔드").company(mockCompany).build();
+
+  @BeforeEach
+  public void init() {
+    doReturn(1L).when(mockCompany).getSeq();
+    doReturn("test company").when(mockCompany).getCompanyName();
+    doReturn("대한민국").when(mockCompany).getCountry();
+    doReturn("서울").when(mockCompany).getRegion();
 
   }
 
   @Test
-  @DisplayName("채용공고 등록")
-  void createRecruitmentNotice() {
-    Company company = Company.builder().companyName("test company").country("한국").region("서울").build();
-    RecruitmentNotice recruitmentNotice = RecruitmentNotice.builder().recruitReward(1000000).recruitDescription("채용 내용").company(company).jobPosition("백엔드 개발자").requiredSkill("Java").build();
+  public void set_recruitment_notice() throws Exception {
+    doReturn(mockCompany).when(this.companyService).get(anyLong());
 
-    RecruitmentNotice save = repository.save(recruitmentNotice);
+    doReturn(RecruitmentNotice.builder().build()).when(this.repository).save(any());
+
+    RecruitmentNoticeVo.Create create = RecruitmentNoticeVo.Create.builder().recruitDescription("채용내용").recruitReward(120000).companySeq(1L).jobPosition("백엔드").requiredSkill("java").build();
+
+    RecruitmentNotice notice = service.set(create);
 
 
+    assertThat(notice).isNotNull();
   }
 
+  @Test
+  public void modify_recruitment_notice() throws Exception {
+    doReturn(Optional.of(recruitmentNotice)).when(repository).findBySeq(anyLong());
+
+    RecruitmentNoticeVo.Update update = RecruitmentNoticeVo.Update.builder().seq(1L).recruitDescription("채용내용").recruitReward(5000).jobPosition("백엔드").requiredSkill("java").build();
+
+    RecruitmentNotice modify = service.modify(update, 1L);
+
+    assertThat(modify.getRecruitReward()).isNotEqualTo(recruitmentNotice.getRecruitReward());
+  }
+
+  @Test
+  public void delete_recruitment_notice() throws Exception {
+
+  }
 
 }
